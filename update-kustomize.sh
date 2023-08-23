4c7e26ce32adfebc00d9b34765f551174dfdadb3
Mormur22 <alejmo12@ucm.es>
2023-08-23 19:42:12 +0200

    primer commit

---
+++ update-kustomize.sh
@@ -0,0 +1,27 @@
+bash
+Copy code
+#!/bin/bash
+
+# Directorios
+SCRIPTS_DIR="./scripts"
+KUSTOMIZE_DIR="./configmaps"
+KUSTOMIZE_FILE="$KUSTOMIZE_DIR/kustomization.yaml"
+
+# Verifica si el encabezado ya existe
+if ! grep -q "configMapGenerator:" $KUSTOMIZE_FILE; then
+    echo "configMapGenerator:" > $KUSTOMIZE_FILE
+fi
+
+# Bucle a través de cada archivo .py y añadirlo al kustomization.yaml
+for script in $SCRIPTS_DIR/*.py; do
+    script_name=$(basename $script .py)
+    # Verifica si el script ya se ha añadido al archivo kustomization.yaml
+    if ! grep -q "$script_name-script" $KUSTOMIZE_FILE; then
+        cat <<EOL >> $KUSTOMIZE_FILE
+- name: $script_name-script
+  namespace: spark-jobs
+  files:
+  - ../scripts/$script_name.py
+EOL
+    fi
+done

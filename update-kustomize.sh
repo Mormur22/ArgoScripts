a842441018600fa68b51c4adc0c392d21128903d
Mormur22 <alejmo12@ucm.es>
2023-08-23 19:45:58 +0200

    correccion erro de creacion archivo

--- update-kustomize.sh
+++ update-kustomize.sh
@@ -3,17 +3,27 @@
 # Directorios
 SCRIPTS_DIR="./scripts"
 KUSTOMIZE_DIR="./configmaps"
+KUSTOMIZE_FILE="$KUSTOMIZE_DIR/kustomization.yaml"
 
-# Encabezado del kustomization.yaml
-echo "configMapGenerator:" > "$KUSTOMIZE_DIR/kustomization.yaml"
+# Crea el archivo si no existe
+touch $KUSTOMIZE_FILE
+
+# Verifica si el encabezado ya existe
+if ! grep -q "configMapGenerator:" $KUSTOMIZE_FILE; then
+    echo "configMapGenerator:" > $KUSTOMIZE_FILE
+fi
 
 # Bucle a través de cada archivo .py y añadirlo al kustomization.yaml
 for script in $SCRIPTS_DIR/*.py; do
     script_name=$(basename $script .py)
-    cat <<EOL >> "$KUSTOMIZE_DIR/kustomization.yaml"
+    # Verifica si el script ya se ha añadido al archivo kustomization.yaml
+    if ! grep -q "$script_name-script" $KUSTOMIZE_FILE; then
+        cat <<EOL >> $KUSTOMIZE_FILE
 - name: $script_name-script
   namespace: spark-jobs
   files:
   - ../scripts/$script_name.py
 EOL
+    fi
 done
+

deb290151d45da022cb02468254aeaf0b16fcd7b
Mormur22 <alejmo12@ucm.es>
2023-08-23 19:44:37 +0200

    error kustomize

--- update-kustomize.sh
+++ update-kustomize.sh
@@ -1,27 +1,19 @@
-bash
-Copy code
 #!/bin/bash
 
 # Directorios
 SCRIPTS_DIR="./scripts"
 KUSTOMIZE_DIR="./configmaps"
-KUSTOMIZE_FILE="$KUSTOMIZE_DIR/kustomization.yaml"
 
-# Verifica si el encabezado ya existe
-if ! grep -q "configMapGenerator:" $KUSTOMIZE_FILE; then
-    echo "configMapGenerator:" > $KUSTOMIZE_FILE
-fi
+# Encabezado del kustomization.yaml
+echo "configMapGenerator:" > "$KUSTOMIZE_DIR/kustomization.yaml"
 
 # Bucle a través de cada archivo .py y añadirlo al kustomization.yaml
 for script in $SCRIPTS_DIR/*.py; do
     script_name=$(basename $script .py)
-    # Verifica si el script ya se ha añadido al archivo kustomization.yaml
-    if ! grep -q "$script_name-script" $KUSTOMIZE_FILE; then
-        cat <<EOL >> $KUSTOMIZE_FILE
+    cat <<EOL >> "$KUSTOMIZE_DIR/kustomization.yaml"
 - name: $script_name-script
   namespace: spark-jobs
   files:
   - ../scripts/$script_name.py
 EOL
-    fi
 done
